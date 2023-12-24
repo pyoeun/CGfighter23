dafd5c2e02ddc7881df10b8807704e0eb0477744
cksdl <cksdl2923@gmail.com>
2023-12-25 06:09:03 +0900

    AddJunSkill

---
+++ SuperSlaves/Assets/Scenes/Park_/Scripts/Skill/Skill19Jun.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill19Jun : MonoBehaviour, ISkill
+{
+    public bool IsDebuffSkill { get; private set; }
+    [SerializeField] private AnimationClip m_skillAnim;
+    private IGameManager m_gameManager;
+    private float m_timer;
+    [SerializeField] private bool m_isP1 = true;
+    [SerializeField] private float m_debuffTime;
+    [SerializeField] private float m_power;
+
+    private void Awake()
+    {
+        m_gameManager = GameObject.Find("IngameManager").GetComponent<IGameManager>();
+    }
+
+    public void Debuff()
+    {
+        if (m_isP1)
+        {
+            m_gameManager.IsAbleMoveP2 = false;
+        }
+        else
+        {
+            m_gameManager.IsAbleMoveP1 = false;
+        }
+
+        Invoke("ExitDebuff", m_debuffTime);
+    }
+
+    public IEnumerator PlaySkill()
+    {
+        float animTime = m_skillAnim.length;
+        m_timer = 0;
+
+        //m_gameManager.IsAbleMove = false;
+        IsDebuffSkill = true;
+
+
+        while (m_timer < animTime)
+        {
+            m_timer += Time.deltaTime;
+            this.transform.Translate((Mathf.Sign(this.transform.localScale.x) * Vector3.right * m_power) * Time.deltaTime);
+            yield return null;
+        }
+
+        //m_gameManager.IsAbleMove = true;
+        IsDebuffSkill = false;
+
+        yield break;
+    }
+
+    private void ExitDebuff()
+    {
+        if (m_isP1)
+        {
+            m_gameManager.IsAbleMoveP2 = true;
+        }
+        else
+        {
+            m_gameManager.IsAbleMoveP1 = true;
+        }
+    }
+}
