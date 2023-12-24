d2a5f204ae0eb604a4084f38d2689ccf38e3c5e2
cksdl <cksdl2923@gmail.com>
2023-12-25 07:14:56 +0900

    AddSeohaSkill

---
+++ SuperSlaves/Assets/Scenes/Park_/Scripts/Skill/Skill12Seoha.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill12Seoha : MonoBehaviour, ISkill
+{
+    public bool IsDebuffSkill { get { return false; } }
+    [SerializeField] private AnimationClip m_skillAnim;
+    private float m_timer;
+    private IGameManager m_gameManager;
+
+    private void Awake()
+    {
+        m_gameManager = GameObject.Find("IngameManager").GetComponent<IGameManager>();
+    }
+
+    public IEnumerator PlaySkill()
+    {
+        float animTime = m_skillAnim.length;
+        m_timer = 0;
+
+        //m_gameManager.IsAbleMove = false;
+
+        while (m_timer < animTime)
+        {
+            m_timer += Time.deltaTime;
+            yield return null;
+        }
+
+        //m_gameManager.IsAbleMove = true;
+
+        yield break;
+    }
+    public void Debuff()
+    {
+
+    }
+}
