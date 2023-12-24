1f356eaff685be7f4fd0903179daff5f101e61be
cksdl <cksdl2923@gmail.com>
2023-12-24 11:06:19 +0900

    UpdatePlayerMovementControl

--- SuperSlaves/Assets/Scenes/Park_/Scripts/GameSystem/TutorialGameManager.cs
+++ SuperSlaves/Assets/Scenes/Park_/Scripts/GameSystem/TutorialGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class TutorialGameManager : MonoBehaviour, IGameManager
 {
@@ -19,6 +20,8 @@ public class TutorialGameManager : MonoBehaviour, IGameManager
     public Boolean[] IsPlayingTutorial { get; private set; }
     public Boolean[] IsReady { get; private set; }
 
+    private Camera m_cam;
+
     //private float m_maxPlayTime = 60f;
     //private float m_ingameTime;
 
@@ -27,6 +30,7 @@ public class TutorialGameManager : MonoBehaviour, IGameManager
 
     private void Awake()
     {
+        m_cam = Camera.main;
         IsPlayingTutorial = new Boolean[System.Enum.GetValues(typeof(TutorialSeq)).Length];
         IsReady = new Boolean[System.Enum.GetValues(typeof(TutorialSeq)).Length];
         for (int i = 0; i < System.Enum.GetValues(typeof(TutorialSeq)).Length; i++)
@@ -47,6 +51,45 @@ public class TutorialGameManager : MonoBehaviour, IGameManager
     private void Update()
     {
         UpdateDistance();
+    }
+
+    public bool IsAbletoMove(int pPlayerType)
+    {
+        Vector3 pos1 = m_cam.WorldToViewportPoint(m_player1.transform.position);
+        Vector3 pos2 = m_cam.WorldToViewportPoint(m_player2.transform.position);
+
+        switch (pPlayerType)
+        {
+            case 1:
+                if ((pos1.x < 0f && pos2.x >= 1f) || (pos1.x > 1f && pos2.x <= 0))
+                {
+                    Vector3 temp1 = new Vector3(Mathf.Clamp(pos1.x, 0, 1), pos1.y, pos1.z);
+                    m_player1.transform.position = m_cam.ViewportToWorldPoint(temp1);
+
+                    return false;
+                }
+                return true;
+            case 2:
+                if ((pos1.x <= 0f && pos2.x > 1f) || (pos1.x >= 1f && pos2.x < 0))
+                {
+                    Vector3 temp2 = new Vector3(Mathf.Clamp(pos2.x, 0, 1), pos2.y, pos2.z);
+                    m_player2.transform.position = m_cam.ViewportToWorldPoint(temp2);
+
+                    return false;
+                }
+                return true;
+            default:
+                Debug.LogError("그럴리가없다");
+                return false;
+        }
+    }
+
+    public void UpdateCamera()
+    {
+        if (IsAbletoMove(1) && IsAbletoMove(2))
+        {
+            m_cam.transform.position = new Vector3((m_player1.transform.position.x + m_player2.transform.position.x) / 2, 0, -10);
+        }
     }
 
     public void UpdateDistance()
